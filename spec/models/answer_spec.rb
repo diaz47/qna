549ad5ceaf6218d4d3c8a13e4e5b959efbc5af59
vlad <icedev97@gmail.com>
2017-02-22 21:29:52 +0300

    Add spec for answer model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe Answer, type: :model do
   it { should belong_to :question }
   it { should belong_to :user }
   it { should validate_presence_of :body }
+
+  describe 'select best answer' do
+    let(:question) { create(:question)}
+    let(:answer) { create(:answer, question: question)}
+    let(:prev_answer) { create(:answer, question: question, best_answer: true)}
+
+    it 'answer should be best' do
+      answer.set_best_answer
+      expect(answer.best_answer).to eq true
+    end
+
+    it 'prev answer should be not best' do
+      answer.set_best_answer
+      prev_answer.reload
+
+      expect(prev_answer.best_answer).to eq false
+    end
+  end
 end
