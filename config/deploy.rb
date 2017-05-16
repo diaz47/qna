5e8f1c0ad1c93b2ba3a0f18c09de7eb15b9a7a83
Vlad Ortin <icedev97@gmail.com>
2017-05-16 21:09:22 +0300

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+# config valid only for current version of Capistrano
+lock "3.8.1"
+
+set :application, "qna"
+set :repo_url, "git@github.com:diaz47/qna.git"
+set :default_env, { rvm_bin_path: "/usr/local/rvm/bin" }
+set :environment, "production"
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/ice/qna"
+set :deploy_user, 'ice'
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", ".env"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"
+
+
+namespace :deploy do
+  desc 'Restart app'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+end
